4cf934f8aaad67e86cdcc0ebb4d56073f5f7a33c
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-11-03 01:11:58 +0900

    update:

--- src/playground_clojure/core.clj
+++ src/playground_clojure/core.clj
@@ -9,3 +9,22 @@
 
 (defn sum [ls]
   (apply + ls))
+
+;; https://japan-clojurians.github.io/clojure-site-ja/
+; マルチアリティ関数
+; デフォルト引数みたいな感じか
+(defn messenger
+  ([]     (messenger "Hello multi arity function"))
+  ([msg]  (println msg)))
+(messenger)
+
+; 可変長引数
+(defn hello [greeting & who]
+  (println greeting who))
+(hello "hello" "world" "class")
+
+; 無名関数
+((fn [msg] (println msg)) "hello!")
+
+;; これと等価: (fn [x y] (+ x y))
+(#(+ %1 %2) 1 2)
